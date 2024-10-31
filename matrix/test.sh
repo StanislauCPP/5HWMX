78fb5e82f53c0aebdd79a1eb768abecf0bc0feb0
StasUbuntus <liver2009@yandex.ru>
2024-10-31 15:37:16 +0300

    determ.cpp: int change to double; add new test t8.test, and answer for this test in matrix/CMakeLists.txt; displace test.sh in matrix directory

--- matrix/test.sh
+++ matrix/test.sh
@@ -1,7 +1,6 @@
 #create test and test matrix via cmake and ctest
 #for unit testing - export TESTOPTION=-DUNITTEST=ON; for testing determinant computing - export TESTOPTION=-DDET=ON
 
-cd ..
 btFolder=testMatrix_build
 rm -rf ${btFolder}
 cmake -S . -B ${btFolder} -DTESTLIB=ON ${TESTOPTION} -DCMAKE_BUILD_TYPE=DEBUG
